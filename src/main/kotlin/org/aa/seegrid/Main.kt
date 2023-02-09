3160097247ecf9c2e08e1db2fb0b72b212ce3018
Antoine Alberti <aa@antoinealberti.com>
2023-02-09 13:35:47 +0100

    sudoku on the right

--- src/main/kotlin/org/aa/seegrid/Main.kt
+++ src/main/kotlin/org/aa/seegrid/Main.kt
@@ -10,7 +10,7 @@ import javafx.stage.Stage
 import nu.pattern.OpenCV
 import org.opencv.core.*
 import org.opencv.imgcodecs.Imgcodecs
-import org.opencv.imgproc.Imgproc
+import org.opencv.imgproc.Imgproc.*
 import org.opencv.objdetect.CascadeClassifier
 import org.opencv.objdetect.Objdetect
 import org.opencv.videoio.VideoCapture
@@ -27,6 +27,22 @@ class FxApp : Application() {
     private var camera: VideoCapture = VideoCapture(0)
 
     override fun start(primaryStage: Stage?) {
+        val (cameraView, workingView) = initView()
+        workingView.setImage(
+            loadImage("src/main/resources/images/sudoku sample.jpg")
+                .toGrayScale()
+                .toFxImage()
+        )
+
+        object : AnimationTimer() {
+            override fun handle(l: Long) {
+                val camShot = captureCamera()
+                cameraView.setImage(camShot.showFaces().toFxImage())
+            }
+        }.start()
+    }
+
+    private fun initView(): Pair<ImageView, ImageView> {
         val cameraView = ImageView()
         val workingView = ImageView()
         val hbox = HBox(cameraView, workingView)
@@ -34,20 +50,17 @@ class FxApp : Application() {
         val stage = Stage()
         stage.setScene(scene)
         stage.show()
-
-        object : AnimationTimer() {
-            override fun handle(l: Long) {
-                val camShot = captureCamera()
-                cameraView.setImage(camShot.showFaces().toFxImage())
-                workingView.setImage(camShot.toGrayScale().toFxImage())
-            }
-        }.start()
+        return Pair(cameraView, workingView)
     }
 
     private fun captureCamera(): Mat {
         val mat = Mat()
         camera.read(mat)
         return mat
+    }
+
+    fun loadImage(imagePath: String): Mat {
+        return Imgcodecs.imread(imagePath)
     }
 }
 
@@ -58,7 +71,7 @@ private fun Mat.showFaces(): Mat {
 private fun Mat.detectFaces(): MatOfRect {
     val cascadeClassifier = CascadeClassifier()
     val minFaceSize = (this.rows() * 0.1f).roundToInt().toDouble()
-    cascadeClassifier.load("src/main/resources/haarcascade_frontalface_alt.xml")
+    cascadeClassifier.load("src/main/resources/models/haarcascade_frontalface_alt.xml")
     val detectedFaces = MatOfRect()
     cascadeClassifier.detectMultiScale(
         this,
@@ -75,24 +88,24 @@ private fun Mat.detectFaces(): MatOfRect {
 private fun Mat.drawRectangles(rectangles: MatOfRect): Mat {
     val enhanced = clone()
     for (rectangle in rectangles.toArray())
-        Imgproc.rectangle(enhanced, rectangle.tl(), rectangle.br(), Scalar(0.0, 0.0, 255.0), 3)
+        rectangle(enhanced, rectangle.tl(), rectangle.br(), Scalar(0.0, 0.0, 255.0), 3)
     return enhanced
 }
 
 private fun Mat.toGrayScale(): Mat =
     transform { source, destination ->
-        Imgproc.cvtColor(
+        cvtColor(
             source,
             destination,
-            Imgproc.COLOR_RGB2GRAY
+            COLOR_RGB2GRAY
         )
     }.transform { source, destination ->
-        Imgproc.adaptiveThreshold(
+        adaptiveThreshold(
             source,
             destination,
             255.0,
-            Imgproc.ADAPTIVE_THRESH_MEAN_C,
-            Imgproc.THRESH_BINARY,
+            ADAPTIVE_THRESH_MEAN_C,
+            THRESH_BINARY,
             31,
             10.0
         )
